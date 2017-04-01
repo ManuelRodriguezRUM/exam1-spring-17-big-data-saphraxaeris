34c61207134fed8e633d80d2876129cfd70fd48f
saphraxaeris <stephan.elias@upr.edu>
2017-04-01 00:47:54 -0400

    fixed problem 1 errors

--- mapreduce/README.md
+++ mapreduce/README.md
@@ -1,1 +1,7 @@
 Folder p1exam1 is the Maven project folder. 
+
+# To Compile
+* Run `mvn package` in project root
+
+# To Run
+* Run `hadoop jar <jar path> <hdfs input file path> <hdfs output dir path>`
--- mapreduce/p1exam1/src/main/java/edu/uprm/cse/bigdata/p1exam1/App.java
+++ mapreduce/p1exam1/src/main/java/edu/uprm/cse/bigdata/p1exam1/App.java
@@ -1,5 +1,8 @@
 package edu.uprm.cse.bigdata.p1exam1;
 
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 
@@ -11,20 +14,25 @@ public class App
             System.err.println("Usage: <input path> <output directory>");
             System.exit(-1);
         }
-
-        Job job = Job.getInstance();
+        try {
+            Job job = Job.getInstance();
             job.setJarByClass(edu.uprm.cse.bigdata.p1exam1.App.class);
             job.setJobName("Exam 1");
 
             FileInputFormat.addInputPath(job, new Path(args[0]));
             FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-            job.setMapperClass(mappers.KeywordsToTweetsMapper.class);
-            job.setReducerClass(reducers.KeywordsToTweetsReducer.class);
+            job.setMapperClass(edu.uprm.cse.bigdata.p1exam1.KeywordsToTweetsMapper.class);
+            job.setReducerClass(edu.uprm.cse.bigdata.p1exam1.KeywordsToTweetsReducer.class);
 
             job.setOutputKeyClass(Text.class);
             job.setOutputValueClass(Text.class);
 
             job.waitForCompletion(true);
+        }
+        catch(Exception ex) {
+            System.err.println(ex.getMessage());
+            System.exit(-1);
+        }
     }
 }
